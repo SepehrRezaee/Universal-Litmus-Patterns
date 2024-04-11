f9f15c096734e2bf6253c7797bc73a84e85f337b
Sepehr Rezaee <78574247+SepehrRezaee@users.noreply.github.com>
2024-04-11 19:05:48 +0330

    Update

--- CIFAR-10/evaluate_ULP.py
+++ CIFAR-10/evaluate_ULP.py
@@ -119,7 +119,7 @@ nofclasses=10 #CIFAR10
 poisoned_models_test = sorted(glob.glob('/kaggle/working/BadNet/*.pth.tar'))
 
 # clean models
-clean_models=glob.glob('/kaggle/input/wanetattack-cifar10/Wanet_dataset/CIFAR10/clean/*.pth.tar')
+clean_models=glob.glob('/kaggle/input/wanetattack-cifar10/Wanet_dataset/CIFAR10/clean/*.pth.tar')[:10]
 
 # val - 100 clean 100 poisoned
 models_test=clean_models + poisoned_models_test

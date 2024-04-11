8756180607aa533e13877d1a272949661eeb5ba0
Sepehr Rezaee <78574247+SepehrRezaee@users.noreply.github.com>
2024-04-11 14:41:32 +0330

    Update

--- CIFAR-10/evaluate_ULP.py
+++ CIFAR-10/evaluate_ULP.py
@@ -86,8 +86,8 @@ if __name__ == "__main__":
 
 os.makedirs("results", exist_ok=True)
 use_cuda=True
-init_num_filters=32
-inter_fc_dim=128
+init_num_filters=64
+inter_fc_dim=384
 nofclasses=10 #CIFAR10
 
 
